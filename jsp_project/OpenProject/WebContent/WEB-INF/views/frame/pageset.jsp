3a1da3558a6903891bd92fb9d888916909ae52f9
pascran <92347552+pascran@users.noreply.github.com>
2021-12-22 02:08:03 +0900

    open

---
+++ jsp_project/OpenProject/WebContent/WEB-INF/views/frame/pageset.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!-- meta -->
+<!-- CSS -->
+<link rel = "stylesheet" href = "${pageContext.request.contextPath}/op/css/default.css">
+<!-- JS -->
+<script src ="${pageContext.request.contextPath}/op/js/index.js"></script>
---
+++ jsp_project/OpenProject/src/jdbc/DriverLoader.java
@@ -0,0 +1,35 @@
+package jdbc;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+public class DriverLoader extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		
+		String driverString = config.getInitParameter("driver");
+		
+		// DB 드라이버 로드
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("mysql 드라이버 로드 성공 !!");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패 !!");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
